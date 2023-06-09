921a5bc7be4c4067815fbea6fce599b14e8885b8
marcusdavidalo <marcusdavidalo@gmail.com>
2023-06-09 19:37:54 +0800

    update AuthController.js and auth.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,7 @@
+// auth.controller.js
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const AuthController = {
   register: async (req, res) => {
@@ -22,14 +24,19 @@ const AuthController = {
       // Save the user to the database
       await newUser.save();
 
-      res.status(201).json({ message: 'User registered successfully' });
+      // Generate an access token
+      const accessToken = generateAccessToken(newUser);
+
+      res
+        .status(201)
+        .json({ message: 'User registered successfully', accessToken });
     } catch (error) {
       res.status(500).json({ error: 'An error occurred' });
     }
   },
 
   login: async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password, rememberMe } = req.body;
 
     try {
       // Find the user by email
@@ -46,11 +53,46 @@ const AuthController = {
         return res.status(401).json({ error: 'Invalid password' });
       }
 
-      res.json({ message: 'Login successful', user });
+      // Generate an access token
+      const accessToken = generateAccessToken(user);
+
+      // Generate a refresh token if "Remember me" is selected
+      const refreshToken = rememberMe ? generateRefreshToken(user) : null;
+
+      res.json({ message: 'Login successful', accessToken, refreshToken });
     } catch (error) {
       res.status(500).json({ error: 'An error occurred' });
     }
   },
 };
+
+// Helper function to generate an access token
+function generateAccessToken(user) {
+  const payload = {
+    userId: user._id,
+    username: user.username,
+    // Include any additional user data you need in the token payload
+  };
+
+  const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+  return accessToken;
+}
+
+// Helper function to generate a refresh token
+function generateRefreshToken(user) {
+  const payload = {
+    userId: user._id,
+    // Include any additional user data you need in the token payload
+  };
+
+  const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: '7d',
+  });
+  // Store the refresh token securely on the server or in a secure HTTP-only cookie
+
+  return refreshToken;
+}
 
 module.exports = AuthController;
