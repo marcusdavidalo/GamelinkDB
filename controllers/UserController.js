04d60092d12991212e954388e2cc403987532e28
Marcus David Alo <marcusdavidalo@gmail.com>
2023-06-29 13:22:01 +0800

    build(deps): update UserController.js, User.js, package-lock.json and users.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,37 +1,28 @@
 const User = require('../models/User');
 
 const UserController = {
-  // Get all users
   getAllUsers: async (req, res) => {
     try {
-      // Retrieve all users from the database
       const users = await User.find();
       res.json(users);
     } catch (error) {
-      // Return an error response if any error occurs
       res.status(500).json({ error: 'An error occurred' });
     }
   },
 
-  // Get a user by ID
   getUserById: async (req, res) => {
     const { id } = req.params;
     try {
-      // Find the user by ID
       const user = await User.findById(id);
       if (!user) {
-        // Return an error response if the user is not found
         return res.status(404).json({ error: 'User not found' });
       }
-      // Return the user object
       res.json(user);
     } catch (error) {
-      // Return an error response if any error occurs
       res.status(500).json({ error: 'An error occurred' });
     }
   },
 
-  // Create a new user
   createUser: async (req, res) => {
     const {
       username,
@@ -45,7 +36,6 @@ const UserController = {
       bio,
     } = req.body;
     try {
-      // Create a new user object with the provided details
       const newUser = new User({
         username,
         email,
@@ -57,17 +47,13 @@ const UserController = {
         avatar,
         bio,
       });
-      // Save the new user to the database
       await newUser.save();
-      // Return the created user object
       res.status(201).json(newUser);
     } catch (error) {
-      // Return an error response if any error occurs
       res.status(500).json({ error: 'An error occurred' });
     }
   },
 
-  // Update a user by ID
   updateUser: async (req, res) => {
     const { id } = req.params;
     const {
@@ -82,7 +68,6 @@ const UserController = {
       bio,
     } = req.body;
     try {
-      // Find the user by ID and update the provided fields
       const user = await User.findByIdAndUpdate(
         id,
         {
@@ -99,52 +84,91 @@ const UserController = {
         { new: true }
       );
       if (!user) {
-        // Return an error response if the user is not found
         return res.status(404).json({ error: 'User not found' });
       }
-      // Return the updated user object
       res.json(user);
     } catch (error) {
-      // Return an error response if any error occurs
       res.status(500).json({ error: 'An error occurred' });
     }
   },
 
-  // Delete a user by ID
   deleteUser: async (req, res) => {
     const { id } = req.params;
     try {
-      // Find the user by ID and delete it from the database
       const user = await User.findByIdAndDelete(id);
       if (!user) {
-        // Return an error response if the user is not found
         return res.status(404).json({ error: 'User not found' });
       }
-      // Return a success message
       res.json({ message: 'User deleted successfully' });
     } catch (error) {
-      // Return an error response if any error occurs
       res
         .status(500)
         .json({ error: 'You are not authorized to delete others accounts' });
     }
   },
+
+  addFollower: async (req, res) => {
+  const { userId, followerId } = req.body;
+  try {
+    console.log(`Adding follower ${followerId} to user ${userId}`);
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    user.followers.push(followerId);
+    await user.save();
+    console.log(`Successfully added follower ${followerId} to user ${userId}`);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+},
+
+
+  removeFollower: async (req, res) => {
+    const { userId, followerId } = req.body;
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      user.followers.pull(followerId);
+      await user.save();
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  },
+
+  addFollowing: async (req, res) => {
+    const { userId, followingId } = req.body;
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      user.following.push(followingId);
+      await user.save();
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  },
+
+  removeFollowing: async (req, res) => {
+    const { userId, followingId } = req.body;
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      user.following.pull(followingId);
+      await user.save();
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  },
 };
 
 module.exports = UserController;
-
-/*
-Explanation:
-
-This UserController module exports an object with several functions that handle user-related operations. Here's a summary of each function:
-
-- getAllUsers: Retrieves all users from the database and returns them as a JSON response.
-
-- getUserById: Takes an ID as a parameter, finds the corresponding user in the database, and returns it as a JSON response. If the user is not found, it returns an error response.
-
-- createUser: Creates a new user based on the provided request body, saves it to the database, and returns the created user as a JSON response. If any error occurs during the process, it returns an error response.
-
-- updateUser: Takes an ID as a parameter, finds the corresponding user in the database, updates the user's fields based on the provided request body, and returns the updated user as a JSON response. If the user is not found, it returns an error response.
-
-- deleteUser: Takes an ID as a parameter, finds the corresponding user in the database, deletes it, and returns a success message. If the user is not found or the requester is not authorized to delete the user, it returns an error response.
-*/
--- models/User.js
+++ models/User.js
@@ -1,89 +1,68 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// Define the user schema
-
-// Create a new mongoose schema for the user model
 const userSchema = new mongoose.Schema({
-  // Username field
   username: {
     type: String,
     unique: true,
     required: true,
   },
-  // Email field
   email: {
     type: String,
     unique: true,
     required: true,
   },
-  // Password field
   password: {
     type: String,
     required: true,
   },
-  // Birthdate field
   birthdate: {
     type: Date,
     required: true,
   },
-  // Social login provider field
   socialLoginProvider: String,
-  // Social login ID field
   socialLoginId: String,
-  // Name field
   name: String,
-  // Avatar field
   avatar: String,
-  // Bio field
   bio: String,
-  // CreatedAt field
   createdAt: {
     type: Date,
     default: Date.now,
   },
-  // UpdatedAt field
   updatedAt: {
     type: Date,
     default: Date.now,
   },
-  // Likes field
   likes: {
     type: Number,
     default: 0,
   },
-  // Followers field
-  followers: {
-    type: Number,
-    default: 0,
-  },
-  // Views field
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }], 
   views: {
     type: Number,
     default: 0,
   },
-  // Admin field
   admin: {
     type: Boolean,
     default: false,
   },
 });
 
-// Hash the password before saving it to the database
-
-// Use the pre-save middleware to hash the password before saving
 userSchema.pre('save', async function (next) {
   try {
-    // Check if the password is modified
     if (!this.isModified('password')) {
       return next();
     }
 
-    // Generate a salt
     const salt = await bcrypt.genSalt(10);
-    // Hash the password using the generated salt
     const hashedPassword = await bcrypt.hash(this.password, salt);
-    // Set the hashed password as the user's password
     this.password = hashedPassword;
     next();
   } catch (error) {
@@ -91,9 +70,6 @@ userSchema.pre('save', async function (next) {
   }
 });
 
-// Hash the password before updating it in the database
-
-// Use the pre-findOneAndUpdate middleware to hash the password before updating it in the database
 userSchema.pre('findOneAndUpdate', async function (next) {
   try {
     if (!this._update.password) {
@@ -108,9 +84,6 @@ userSchema.pre('findOneAndUpdate', async function (next) {
   }
 });
 
-// Custom method to compare the provided password with the hashed password
-
-// Add a custom method to the user schema to compare passwords
 userSchema.methods.comparePassword = async function (password) {
   try {
     return await bcrypt.compare(password, this.password);
@@ -119,27 +92,6 @@ userSchema.methods.comparePassword = async function (password) {
   }
 };
 
-// Create the User model using the user schema
-
-// Create the User model using the user schema and name it 'User'
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
-/*
-Explanation:
-
-- The code above defines the user model using Mongoose.
-
-- The user model represents a user entity in the application, with various fields such as username, email, password, birthdate, etc.
-
-- The userSchema variable holds the Mongoose schema definition for the user model.
-
-- Each field in the schema is defined with its respective type and any additional properties such as uniqueness, required status, and default values.
-
-- The userSchema.pre('save', ...) method is a pre-save middleware that is executed before saving a user to the database. It hashes the password using bcrypt.
-
-- The userSchema.methods.comparePassword(...) method is a custom method added to the user schema. It compares the provided password with the hashed password stored in the database.
-
-- Finally, the User model is created using mongoose.model('User', userSchema), and the model is exported to be used by other parts of the application.
-*/
